619a8fdf85f15f3dce6bc9587dbabcf1052807c4
seaneiros <enigmatic.gate@gmail.com>
2015-09-15 20:59:12 +0500

    multiple kind-select

--- js/AppController.js
+++ js/AppController.js
@@ -14,7 +14,7 @@ angular.module('PonyShop', ['ui-rangeSlider'])
 
     $scope.filterValues = {
       color   : null,
-      kind    : null,
+      kind    : [],
       priceMin: null,
       priceMax: null,
       is_new  : null
@@ -31,192 +31,196 @@ angular.module('PonyShop', ['ui-rangeSlider'])
     $scope.selectPonies = function(item) {
       return (
         (item.color === ($scope.filterValues.color || item.color)) &&
-        (item.kind === ($scope.filterValues.kind || item.kind)) &&
+        (!$scope.filterValues.kind.length || $scope.filterValues.kind.indexOf(item.kind) >= 0) &&
         (item.price >= $scope.filterValues.priceMin) &&
         (item.price <= $scope.filterValues.priceMax) &&
-        (item.is_new === ($scope.filterValues.is_new === null ? item.is_new : $scope.filterValues.is_new))
+        (item.is_new === ($scope.filterValues.is_new  ? true : item.is_new))
       );
     };
 
-    $http.get('#')
-      .success(function (data, status, headers, config) {
-        $scope.ponyData = [
-          {
-            "name"  : "Твайлайт",
-            "color" : "Фиолетовый",
-            "kind"  : "Единорог",
-            "price" : 19.99,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 1",
-            "color" : "Розовый",
-            "kind"  : "Земная пони",
-            "price" : 10.54,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 2",
-            "color" : "Синий",
-            "kind"  : "Пегас",
-            "price" : 3.21,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 3",
-            "color" : "Синий",
-            "kind"  : "Пегас",
-            "price" : 5.89,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 4",
-            "color" : "Зеленый",
-            "kind"  : "Пегас",
-            "price" : 15.95,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 5",
-            "color" : "Розовый",
-            "kind"  : "Земная пони",
-            "price" : 12.05,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 6",
-            "color" : "Розовый",
-            "kind"  : "Апикорн",
-            "price" : 1.15,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 7",
-            "color" : "Розовый",
-            "kind"  : "Апикорн",
-            "price" : 99.50,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 8",
-            "color" : "Розовый",
-            "kind"  : "Земная пони",
-            "price" : 43.23,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 9",
-            "color" : "Зеленый",
-            "kind"  : "Апикорн",
-            "price" : 23.65,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 10",
-            "color" : "Синий",
-            "kind"  : "Земная пони",
-            "price" : 64.29,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 11",
-            "color" : "Зеленый",
-            "kind"  : "Земная пони",
-            "price" : 67.30,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 12",
-            "color" : "Зеленый",
-            "kind"  : "Пегас",
-            "price" : 54.78,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 13",
-            "color" : "Розовый",
-            "kind"  : "Пегас",
-            "price" : 13.34,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 14",
-            "color" : "Синий",
-            "kind"  : "Земная пони",
-            "price" : 12.10,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 15",
-            "color" : "Розовый",
-            "kind"  : "Пегас",
-            "price" : 12.10,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 16",
-            "color" : "Розовый",
-            "kind"  : "Пегас",
-            "price" : 12.10,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 17",
-            "color" : "Синий",
-            "kind"  : "Земная пони",
-            "price" : 87.90,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 18",
-            "color" : "Розовый",
-            "kind"  : "Апикорн",
-            "price" : 9.99,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 19",
-            "color" : "Розовый",
-            "kind"  : "Земная пони",
-            "price" : 9.99,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 20",
-            "color" : "Зеленый",
-            "kind"  : "Апикорн",
-            "price" : 9.99,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 21",
-            "color" : "Зеленый",
-            "kind"  : "Апикорн",
-            "price" : 11.11,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 22",
-            "color" : "Розовый",
-            "kind"  : "Апикорн",
-            "price" : 199.90,
-            "is_new": true
-          },
-          {
-            "name"  : "Тестовый пони 23",
-            "color" : "Розовый",
-            "kind"  : "Земная пони",
-            "price" : 17.80,
-            "is_new": false
-          },
-          {
-            "name"  : "Тестовый пони 24",
-            "color" : "Синий",
-            "kind"  : "Апикорн",
-            "price" : 17.80,
-            "is_new": true
-          }
-        ];
-      });
+
+    /**
+     * Здесь должен был бы быть вызов сервиса,
+     * но чтобы не привязываться к серверу, просто
+     * захардкодим исходные данные
+     */
+
+    $scope.ponyData = [
+      {
+        "name"  : "Твайлайт",
+        "color" : "Фиолетовый",
+        "kind"  : "Единорог",
+        "price" : 19.99,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 1",
+        "color" : "Розовый",
+        "kind"  : "Земная пони",
+        "price" : 10.54,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 2",
+        "color" : "Синий",
+        "kind"  : "Пегас",
+        "price" : 3.21,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 3",
+        "color" : "Синий",
+        "kind"  : "Пегас",
+        "price" : 5.89,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 4",
+        "color" : "Зеленый",
+        "kind"  : "Пегас",
+        "price" : 15.95,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 5",
+        "color" : "Розовый",
+        "kind"  : "Земная пони",
+        "price" : 12.05,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 6",
+        "color" : "Розовый",
+        "kind"  : "Апикорн",
+        "price" : 1.15,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 7",
+        "color" : "Розовый",
+        "kind"  : "Апикорн",
+        "price" : 99.50,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 8",
+        "color" : "Розовый",
+        "kind"  : "Земная пони",
+        "price" : 43.23,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 9",
+        "color" : "Зеленый",
+        "kind"  : "Апикорн",
+        "price" : 23.65,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 10",
+        "color" : "Синий",
+        "kind"  : "Земная пони",
+        "price" : 64.29,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 11",
+        "color" : "Зеленый",
+        "kind"  : "Земная пони",
+        "price" : 67.30,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 12",
+        "color" : "Зеленый",
+        "kind"  : "Пегас",
+        "price" : 54.78,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 13",
+        "color" : "Розовый",
+        "kind"  : "Пегас",
+        "price" : 13.34,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 14",
+        "color" : "Синий",
+        "kind"  : "Земная пони",
+        "price" : 12.10,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 15",
+        "color" : "Розовый",
+        "kind"  : "Пегас",
+        "price" : 12.10,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 16",
+        "color" : "Розовый",
+        "kind"  : "Пегас",
+        "price" : 12.10,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 17",
+        "color" : "Синий",
+        "kind"  : "Земная пони",
+        "price" : 87.90,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 18",
+        "color" : "Розовый",
+        "kind"  : "Апикорн",
+        "price" : 9.99,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 19",
+        "color" : "Розовый",
+        "kind"  : "Земная пони",
+        "price" : 9.99,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 20",
+        "color" : "Зеленый",
+        "kind"  : "Апикорн",
+        "price" : 9.99,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 21",
+        "color" : "Зеленый",
+        "kind"  : "Апикорн",
+        "price" : 11.11,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 22",
+        "color" : "Розовый",
+        "kind"  : "Апикорн",
+        "price" : 199.90,
+        "is_new": true
+      },
+      {
+        "name"  : "Тестовый пони 23",
+        "color" : "Розовый",
+        "kind"  : "Земная пони",
+        "price" : 17.80,
+        "is_new": false
+      },
+      {
+        "name"  : "Тестовый пони 24",
+        "color" : "Синий",
+        "kind"  : "Апикорн",
+        "price" : 17.80,
+        "is_new": true
+      }
+    ];
 
   }]);
